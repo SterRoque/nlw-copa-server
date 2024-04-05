5c5c54209ba40bff24c9bcfec69284e024848aad
Sterphane Roque <stermiss2@gmail.com>
2024-04-05 01:00:15 -0300

    feat: inital services

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client"
 import Fastify from "fastify"
 import cors from "@fastify/cors"
+import { z } from 'zod'
+import ShortUniqueId from "short-unique-id"
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -17,6 +19,35 @@ async function bootstrap() {
     fastify.get("/pools/count", async () => {
         const count = await prisma.pool.count()
         return { count }
+    })
+
+    fastify.get("/users/count", async () => {
+        const count = await prisma.pool.count()
+        return { count }
+    })
+
+    fastify.get("/guesses/count", async () => {
+        const count = await prisma.guess.count()
+        return { count }
+    })
+
+    fastify.post("/pools", async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string()
+        })
+
+        const { title } = createPoolBody.parse(request.body)
+        const generate = new ShortUniqueId({ length: 6 }).rnd()
+        const code = String(generate).toUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code,
+            }
+        })
+
+        return reply.status(201).send({ code })
     })
 
     await fastify.listen({ port: 3333 })
